2ecc0b66f9096dc4b7593a9835af12d72db10471
APdrone <amanpatwalfor@gmail.com>
2022-02-13 12:50:10 +0530

    changed pizza controller  to caps

--- routes/pizzaRoutes.js
+++ routes/pizzaRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const pizzaController = require("../Controllers/pizzaController");
+const pizzaController = require("../Controllers/PizzaController");
 const authController = require("../Controllers/authController");
 
 const router = express.Router();
